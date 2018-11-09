d8c6d1db8a50b050f74b97f0349a1cf851c939d0
Talytta Carvalho <talytta110@hotmail.com>
2018-11-09 11:44:08 -0200

    pequenas alteracoes de css

--- dist/js/insurance.js
+++ dist/js/insurance.js
@@ -1,12 +1,11 @@
 const renderInsurance = () => {
   return `
-  <div class="">
-    <h2>SEGUROS</h2>
-    <div class="d-flex">
-      <p class="text-size-1-5">Conte com as vantagens dos nossos seguros.</p>
-      <p class="text-size-1-5">Para melhor atender você, oferecemos desde produtos simples até os mais completos. <br> O nosso compromisso é lhe oferecer os melhores benefícios.</p>
+  <div class="mx-5">
+    <h2 class="mt-4">SEGUROS</h2>
+    <div class="">
+      <p class="text-size-1-5">O nosso compromisso é lhe oferecer os melhores benefícios. Conte com as vantagens dos nossos seguros.</p>
     </div>
-    <p class="text-size-1-5">Selecione abaixo o seguro desejado para ver mais detalhes</p>
+    <p class="text-size-1-5">Selecione abaixo o seguro desejado para ver mais detalhes:</p>
     
     <div class="tab">
       <button id="house" class="tablinks" onclick="openCity(event, 'Residencial')">Residencial</button>
@@ -16,54 +15,82 @@ const renderInsurance = () => {
     
     <div id="Residencial" class="tabcontent">
       <h3>Seguro Residencial</h3>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-4">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Cobertura básica: R$350,00</h5>
-                <p class="card-text">Com essa cobertura você pode contar com: encanador, chaveiro, vidraceiro e eletricista</p>
-                <a href="/confirmation" class="btn btn-primary">Contratar</a>
-                </div>
-            </div>
-          </div>
-          <div class="col-lg-4">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Cobertura completa: R$1500,00</h5>
-                <p class="card-text">Na cobertura completa conte com: encanador, chaveiro, vidraceiro, eletricista, instalação de ventilador de teto e substituição de telha.</p>
-                <a href="/confirmation" class="btn btn-primary">Contratar</a>
-                </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <table class="table">
+        <thead class="bg-primary">
+          <tr>
+            <th scope="col">Valor do Seguro</th>
+            <th scope="col">Valor Mensal</th>
+            <th scope="col">Validade</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">R$ 2.500</th>
+            <td>R$ 250,00</td>
+            <td>12 meses</td>
+            <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
+          </tr>
+          <tr>
+            <th scope="row">R$ 2.500</th>
+            <td>R$ 175,00</td>
+            <td>18 meses</td>
+            <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
+          </tr>
+          <tr>
+            <th scope="row">R$ 2.500</th>
+            <td>R$ 150,00</td>
+            <td>24 meses</td>
+            <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
+          </tr>
+          <tr>
+            <th scope="row">R$ 2.500</th>
+            <td>R$ 125,00</td>
+            <td>30 meses</td>
+            <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 
     <div id="Vida" class="tabcontent">
     <h3>Seguro de Vida</h3>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Cobertura básica: R$350,00</h5>
-              <p class="card-text">Com essa cobertura você pode contar com: encanador, chaveiro, vidraceiro e eletricista</p>
-              <a href="/confirmation" class="btn btn-primary">Contratar</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4">
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Cobertura complera: R$1500,00</h5>
-              <p class="card-text">Na cobertura completa conte com: encanador, chaveiro, vidraceiro, eletricista, instalação de ventilador de teto e substituição de telha.</p>
-              <a href="/confirmation" class="btn btn-primary">Contratar</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      <table class="table">
+        <thead class="bg-primary">
+          <tr>
+            <th scope="col">Valor do Seguro</th>
+            <th scope="col">Valor Mensal</th>
+            <th scope="col">Validade</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">R$ 3.000</th>
+            <td>R$ 300,00</td>
+            <td>12 meses</td>
+            <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
+          </tr>
+          <tr>
+            <th scope="row">R$ 3.000</th>
+            <td>R$ 150,00</td>
+            <td>18 meses</td>
+            <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
+          </tr>
+          <tr>
+            <th scope="row">R$ 3.000</th>
+            <td>R$ 120,00</td>
+            <td>24 meses</td>
+            <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
+          </tr>
+          <tr>
+            <th scope="row">R$ 3.000</th>
+            <td>R$ 125,00</td>
+            <td>30 meses</td>
+            <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
+          </tr>
+        </tbody>
+      </table>
     </div>
 
     <div id="Auto" class="tabcontent">
@@ -76,6 +103,7 @@ const renderInsurance = () => {
           <th scope="col">Ano</th>
           <th scope="col">Parcelas</th>
           <th scope="col">Total</th>
+          <th></th>
         </tr>
       </thead>
     <tbody>
@@ -85,6 +113,7 @@ const renderInsurance = () => {
         <td>2014</td>
         <td>ate 3X S/JUROS</td>
         <td>R$ 1.500,00</td>
+        <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
       </tr>
       <tr>
           <th scope="row">Gol 1.6 Mi I MOTION T. Flex 8V 4P</th>
@@ -92,6 +121,7 @@ const renderInsurance = () => {
           <td>2014</td>
           <td>ate 6X JUROS</td>
           <td>R$ 2.100,00</td>
+          <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
       </tr>
       <tr>
           <th scope="row">Gol 1.6 Mi I MOTION T. Flex 8V 4P</th>
@@ -99,14 +129,13 @@ const renderInsurance = () => {
           <td>2014</td>
           <td>ate 12 X JUROS</td>
           <td>R$ 3.000,00</td>
+          <td><a href="/confirmation" class="text-light btn btn-primary p-3 mr-3 btn-font-size font-strong">Contratar</a></td>
        </tbody>
      </table>
     </div>
 
     <div>
-      <p>Interessado em saber mais sobre nossos serviços?</p>
-      <p>Entre em contato com a gente pelo telefone: (xx) 555-0000.</p>
-      <p>Ou venha em uma de nossas agências.</p>
+      <p>Gostou dos novos serviços? Vá até uma de nossas <a href="/map">AGÊNCIAS</a>.</p>
   </div>
 
   <div class="d-flex justify-content-center">
