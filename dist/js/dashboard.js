09a42e2fc1e74873a412562231b26cd02497cc58
Alinnimp <linnimonteirop@gmail.com>
2018-11-09 00:59:01 -0200

    alt index/css

--- dist/js/dashboard.js
+++ dist/js/dashboard.js
@@ -1,18 +1,25 @@
 const renderDashboard = () => {
   return `
     <section id="dashboard-btns">
-      <div class="btn">
-        <a href="/payments" class="">Efetue pagamentos</a>
+      <div class="container">
+        <div class="d-flex justify-content-between flex-wrap">
+          <div id="btns" class="col-md-5 col-sm-12 p-5 btn d-flex btn-outline-light justify-content-between mt-5">
+            <a href="/payments" class="text-dark">Faça um pagamento</a>
+          </div>
+          <div id="btns" class="col-md-5  col-sm-12 p-5 btn btn-outline-light d-flex mt-5">
+            <a href="/loan" class="text-dark">Faça um empréstimo</a>
+          </div>
+        </div>
+        <div class="d-flex justify-content-between flex-wrap">
+          <div id="btns" class="col-md-5 sm-6 p-5 btn d-flex btn-outline-light justify-content-between mt-5">
+            <a href="/insurance" class="text-dark">Faça um seguro</a>
+          </div>
+          <div id="btns" class="col-md-5 sm-6 p-5 btn d-flex justify-content-between  mt-5">
+            <a href="/map" class="text-dark">Encontre uma agência <br>próxima a você</a>
+          </div>
+        </div>
       </div>
-      <div class="btn">
-        <a href="/loan" class="">Simule um empréstimo</a>
-      </div>
-      <div class="btn">
-        <a href="/insurance" class="">Simule um seguro</a>
-      </div>
-      <div class="btn">
-        <a href="/map" class="">Encontre uma agência próxima a você</a>
-      </div>
+
     </section>
   `;
 };
