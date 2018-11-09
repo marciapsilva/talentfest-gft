f23688de82a9e95ffcdf74c47ba5ef861e22a393
Talytta Carvalho <talytta110@hotmail.com>
2018-11-09 06:04:12 -0200

    add insurance, login and correction of some errors

--- dist/js/insurance.js
+++ dist/js/insurance.js
@@ -1,8 +1,126 @@
 const renderInsurance = () => {
   return `
   <div class="">
-    <p>Carrega a página de SEGUROS</p>
-    <a href="/" class="">Voltar</a>
+    <h2>SEGUROS</h2>
+    <p>Conte com as vantagens dos nossos seguros.</p>
+    <p>Para melhor atender você, oferecemos desde produtos simples até os mais completos. O nosso compromisso é lhe oferecer os melhores benefícios.</p>
+    
+    <div class="tab">
+      <button id="house" class="tablinks" onclick="openCity(event, 'Residencial')">Residencial</button>
+      <button id="life" class="tablinks" onclick="openCity(event, 'Vida')">Vida</button>
+      <button id="car" class="tablinks" onclick="openCity(event, 'Auto')">Auto</button>
+    </div>
+    
+    <div id="Residencial" class="tabcontent">
+      <h3>Seguro Residencial</h3>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Cobertura básica: R$350,00</h5>
+                <p class="card-text">Com essa cobertura você pode contar com: encanador, chaveiro, vidraceiro e eletricista</p>
+                <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Cobertura básica: R$1500,00</h5>
+                <p class="card-text">Na cobertura completa conte com: encanador, chaveiro, vidraceiro, eletricista, instalação de ventilador de teto e substituição de telha.</p>
+                <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Cobertura básica: R$3000,00</h5>
+                <p class="card-text">Direito a comer banana Na cobertura completa conte com: encanador, chaveiro, vidraceiro, eletricista, instalação de ventilador de teto e substituição de telha.</p>
+                <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="Vida" class="tabcontent">
+    <h3>Seguro de Vida</h3>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Cobertura básica: R$350,00</h5>
+              <p class="card-text">Com essa cobertura você pode contar com: encanador, chaveiro, vidraceiro e eletricista</p>
+              <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Cobertura básica: R$1500,00</h5>
+              <p class="card-text">Na cobertura completa conte com: encanador, chaveiro, vidraceiro, eletricista, instalação de ventilador de teto e substituição de telha.</p>
+              <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Cobertura básica: R$3000,00</h5>
+              <p class="card-text">Direito a comer banana Na cobertura completa conte com: encanador, chaveiro, vidraceiro, eletricista, instalação de ventilador de teto e substituição de telha.</p>
+              <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+
+    <div id="Auto" class="tabcontent">
+          <h3>Seguro Auto</h3>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Cobertura básica: R$350,00</h5>
+                    <p class="card-text">Com essa cobertura você pode contar com: encanador, chaveiro, vidraceiro e eletricista</p>
+                    <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Cobertura básica: R$1500,00</h5>
+                    <p class="card-text">Na cobertura completa conte com: encanador, chaveiro, vidraceiro, eletricista, instalação de ventilador de teto e substituição de telha.</p>
+                    <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-4">
+                <div class="card">
+                  <div class="card-body">
+                    <h5 class="card-title">Cobertura básica: R$3000,00</h5>
+                    <p class="card-text">Direito a comer banana Na cobertura completa conte com: encanador, chaveiro, vidraceiro, eletricista, instalação de ventilador de teto e substituição de telha.</p>
+                    <a href="/confirmation/350" class="btn btn-primary">Contratar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    </div>
+
+    <div>
+      <p>Interessado em saber mais sobre nossos serviços?</p>
+      <p>Entre em contato com a gente pelo telefone: (xx) 555-0000.</p>
+      <p>Ou venha em uma de nossas agências.</p>
   </div>
+  <a href="/dashboard" class="btn btn-secondary">Voltar</a>
   `;
 };
--- dist/js/routes.js
+++ dist/js/routes.js
@@ -1,20 +1,54 @@
 $(document).ready(() => {
-  page('/', dashboard);
+  page('/', login);
+  page('/dashboard', dashboard);
   page('/payments', payments);
   page('/loan', loan);
   page('/insurance', insurance);
   page('/map', map);
+  page('/confirmation/:valor', confirmation)
   page('/successful_operation', successfulOperation);
 
   page();
 })
 
+function login() {
+  $('nav').html("");
+  $('main').html(renderLogin());
+  $('#mapContainer').hide();
+  $('#banana').hide();
+
+  $('#botaoLogin').click(function(e){
+    const nome = $("#loginName").val();
+    const agencia = $("#loginAg").val();
+    const conta = $("#loginCC").val();
+    localStorage.setItem("nome", nome);
+    localStorage.setItem("agencia", agencia);
+    localStorage.setItem("conta", conta);
+    page('/dashboard')
+    e.preventDefault()
+  })
+}
+
 function dashboard() {
+  const nome = localStorage.getItem("nome");
+  const agencia = localStorage.getItem("agencia");
+  const conta = localStorage.getItem("conta");
+  $('nav').html(renderNavBar(nome, agencia, conta));
   $('main').html(renderDashboard());
+  $('#botaoLogout').click(function(e){
+    localStorage.clear();
+    page('/')
+    e.preventDefault()
+  })
+}
+
+function confirmation(ctx) {
+  $('main').html(renderConfirmation(ctx.params.valor));
 }
 
 function payments() {
   $('main').html(renderPayments());
+
 }
 
 function loan() {
@@ -23,6 +57,12 @@ function loan() {
 
 function insurance() {
   $('main').html(renderInsurance());
+  $('#bananaoButao').click(e => {
+    page("/confirmation");
+    e.preventDefault();
+  });
+
+  
 }
 
 function map() {
